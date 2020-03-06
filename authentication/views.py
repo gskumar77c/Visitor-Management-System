8d236a769a48bdfcf55afb18f808efafa2ae8ec3
sk <2017meb1208@iitrpr.ac.in>
2020-03-07 02:26:35 +0530

    error handling

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
 	return render(request,'home.html')
@@ -11,8 +12,18 @@ def home(request):
 def register(request):
 	if request.method == 'POST':
 		form = UserCreationForm(request.POST)
-		form.save()
-		return redirect('login')
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+		else:
+			usr = form.cleaned_data.get('email')
+			if usr==None:
+				messages.warning(request, 'Invalid email address')
+			else :
+				pas1 = form.cleaned_data.get('password1')
+				pas2 = form.cleaned_data.get('password2')
+				if(pas1 != pas2) :
+					messages.warning(request, 'confirm password must be same as password')
 
 	form = UserCreationForm()
 	return render(request,'register.html',{'form':form})
